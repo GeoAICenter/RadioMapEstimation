0d5bded3e9dc2ed6b7387808fdc5d9dd0e54497c
WilliamLockeIV <williamlockeiv@gmail.com>
2023-08-12 21:26:11 -0500

    import torch.nn in drop_path.py

--- util/drop_path.py
+++ util/drop_path.py
@@ -7,6 +7,7 @@ Original license is included in the file "Apache License" in the root directory 
 '''
 
 import torch
+import torch.nn as nn
 
 def drop_path(x, drop_prob: float = 0., training: bool = False):
     """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
